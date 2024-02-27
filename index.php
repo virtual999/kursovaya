56fc46f94d6abf9137307cad21cd3220070c07d6
Akagami no Shanks <89997083+MustafaAlibekov@users.noreply.github.com>
2024-02-27 12:31:50 +0300

    lol

---
+++ index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Интернет-магазин kinder-shop</title>
+    <link rel="icon" href="images/logo.jpg">
+    <link rel="stylesheet" href="styles/main.css">
+</head>
+<body>
+    <header>
+        <img width="100px" height="100px" src="images/logo.jpg" alt="Логотип">
+        <h1>Интернет-магазин kinder-shop</h1>
+    </header>
+    <nav>
+        <a href="/">Главная</a>
+        <a href="/admpanel">Административная панель</a>
+    </nav>
+    <main>
+        <p class="error"> </p>
+        <form>
+            <label>
+                Логин
+                <input type="text" name="login">
+            </label>
+            <label>
+                Пароль
+                <input type="password" name="password">
+            </label>
+            <button>Войти</button>
+        </form>
+    </main>
+</body>
+</html>
